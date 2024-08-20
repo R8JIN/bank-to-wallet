4a2b58438deb45617f2d836a48ae32aedf9ad8e6
R8JIN <57449596+R8JIN@users.noreply.github.com>
2024-08-20 10:20:26 +0545

    Add role then assign

--- src/app/components/user-profile-log/user-profile-log.component.ts
+++ src/app/components/user-profile-log/user-profile-log.component.ts
@@ -5,6 +5,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FormMessageComponent } from '../../form-message/form-message.component';
 import { HighlightDirective } from '../../attributes/highlight.directive';
+import { sample } from 'rxjs';
 
 @Component({
   selector: 'app-user-profile-log',
@@ -21,39 +22,55 @@ export class UserProfileLogComponent {
   route = inject(ActivatedRoute);
   userService = inject(UserService);
   user_role:any;
+  roleList:any = [];
 
+  availableRoles: string[] =[];
   successMessage!:string;
   
   constructor(private router: Router){
     this.userProfileId = parseInt(this.route.snapshot.params['id'], 10);
     this.user_role= new Map([
-      ["ROLE_ADMIN", "Admin"],
-      ["ROLE_MODERATOR", "Moderator"],
-      ["ROLE_USER", "User"]
+      ["ROLE_ADMIN", "admin"],
+      ["ROLE_MODERATOR", "mod"],
+      ["ROLE_USER", "user"]
     ]);
+    this.availableRoles = ['admin', 'user', 'mod']; 
 
   }
 
   ngOnInit(){
     this.userService.getUserDetail(this.userProfileId).subscribe((response:any)=>{
-      this.data = response.data;
+      this.data = response?.data;
+      let sampleRole:any = [];
+      this.data.roles.forEach((element:any) => {
+        sampleRole.push(this.user_role.get(element.name));
+      });
+
+      console.log("The sample role are ", sampleRole);
+      this.availableRoles = this.availableRoles.filter(role => !sampleRole.includes(role));
       console.log("The profile detail is ", this.data);
     })
   }
 
-  newRole: string = ''; // To hold the selected role
-  availableRoles: string[] = ['admin', 'user', 'mod']; // Example available roles
+  newRole: string = ''; 
 
-  assignRole(role: string) {   
+
+  addRole(role: string) {   
+    this.newRole = '';
     console.log(`Assigning role: ${role}`);
-    let roleList = [];
-    roleList.push(role);
+    this.roleList.push(role);
+    this.availableRoles.splice(this.availableRoles.indexOf(role), 1);
+    console.log("The available Roles", this.availableRoles);
     
-    this.userService.assignNewRole(this.userProfileId, roleList).subscribe((response:any)=>{
+
+      
+  }
+
+  assignRole(){
+    this.userService.assignNewRole(this.userProfileId, this.roleList).subscribe((response:any)=>{
       console.log("The response ", response);
       this.successMessage = "New role assign";
     })
-      
   }
 
   toggleEditMode(){
